0396cc9dd8f234a6ec9522a4dc65a303c00e92ff
iriszhang <zhang.tianyi13@northeastern.edu>
2024-11-11 00:22:33 -0800

    fourth commit

--- SheSense/app/backend_server/app.js
+++ SheSense/app/backend_server/app.js
@@ -10,17 +10,14 @@ const communitySupportRoute = require('./routes/communitySupportRoute');
 const healthMonitoringRoute = require('./routes/healthMonitoringRoute');
 const mentalHealthRoutes = require('./routes/mentalHealthRoute');
 
-
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
 
 // Mount routes at different paths
 app.use('/api', symptomCheckerRoute);  // Handles routes under /api/symptom-checker
-app.use('/api', moodTrackingRoute);    // Handles routes under /api/mood-tracking
 app.use('/api', communitySupportRoute); // Handles routes under /api/community-support
 app.use('/api', healthMonitoringRoute); // Handles routes under /api/health-monitoring
 app.use('/api/mentalHealth', mentalHealthRoutes); // Mental health route
-
 
 // Root endpoint for health check or other info (optional)
 app.get('/', (req, res) => {
