56921ff57c0920a2431613df0be9673a90bc0b67
faithzhang <tianxinfaith720@gmail.com>
2024-11-10 23:57:55 -0800

    config and requirements

---
+++ SheSense/config.py
@@ -0,0 +1,11 @@
+PERSPECTIVE_API_KEY = 'API_KEY'
+
+# Notion Integration
+NOTION_TOKEN = 'notion_integration_token'
+NOTION_DATABASE_ID = 'shesense_notion_database_id'
+
+# Confluence Integration
+CONFLUENCE_URL = 'https://shesense-domain.atlassian.net/wiki/rest/api/content/'
+CONFLUENCE_USERNAME = 'shesense@example.com'
+CONFLUENCE_API_TOKEN = 'shesense_confluence_api_token'
+
